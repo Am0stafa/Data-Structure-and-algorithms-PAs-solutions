eff35260612095cfc3676c67370e322f8c1dc387
Abdelrahman mostafa <a.abdo.mae@gmail.com>
2021-12-02 22:36:05 +0200

    before solving

---
+++ src/YoghurtProduct.java
@@ -0,0 +1,29 @@
+import java.util.Date;
+
+public class YoghurtProduct implements Comparable {
+	public int id;
+	public double originalPrice;
+	public Date expiryDate;
+	
+	
+	public YoghurtProduct(int price, int id, Date expiryDate) {
+		
+		this.originalPrice = price;
+		this.id = id;
+		this.expiryDate = expiryDate;
+	}
+
+
+	@Override
+	public int compareTo(Object o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+		
+		
+	
+		
+	
+	
+}
